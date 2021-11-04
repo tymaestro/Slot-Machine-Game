3242514f9bcb847c4f64004a24b119de3907d1ab
tymaestro <tymaestro22@gmail.com>
2021-11-04 15:49:38 +0000

    reset html and css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
+// function change() {
+//     let button = document.getElementsByClassName('num-button');
+//     button.style.width = "80px";
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let buttons = document.getElementsByTagName("button");
+//     buttons.style.color = "green";
+// });
+
 function change() {
     let dog = document.getElementById("dog");
-    dog.style.width = "200px";
+    dog.style.color = "green";
 }
