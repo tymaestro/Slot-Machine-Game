8b400499f18c400eb1c7412503a626ce988043a3
tymaestro <tymaestro22@gmail.com>
2021-11-05 16:57:56 +0000

    test javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,27 +8,63 @@
 //     buttons.style.color = "green";
 // });
 
-function change() {
-    let dog = document.getElementById("dog");
-    dog.style.color = "green";
-}
+// // Test Code
 
-function startGame() {
+// function change() {
+//     let dog = document.getElementById("dog");
+//     dog.style.color = "green";
+// }
 
-}
-
-function verifyAnswer() {
-
-}
-
-function displaySlotQuestion() {
-
-}
-
-function correctScore() {
-
-}
-
-function incorrectScore() {
+// function change() {
+//     let playerOne = document.getElementById("player1");
+//     let playerTwo = document.getElementById("player2");
+//     let playerThree = document.getElementById("player3");
+//     let computerOne = document.getElementById("computer1");
+//     let computerTwo = document.getElementById("computer2");
+//     let computerThree = document.getElementById("computer3");
+//     playerOne.style.color = "green";
+//     playerTwo.style.color = "blue";
+//     playerThree.style.color = "purple";
+//     let playerGroup = [playerOne, playerTwo, playerThree];
+//     let computerGroup = [computerOne, computerTwo, computerThree];
+//     if (playerGroup === computerGroup) {
+//         alert("Congratulations! You're a winner!");
+//     } else {
+//         alert("I'm sorry, better luck next time!");
+//     }
     
+// }
+
+// function getRandomNumber() {
+//     Math.floor(Math.random() * 3);
+// }
+
+// // Real Code
+
+// function startGame() {
+
+// }
+
+// function verifyAnswer() {
+
+// }
+
+// function displaySlotQuestion() {
+
+// }
+
+// function correctScore() {
+
+// }
+
+// function incorrectScore() {
+
+// }
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 3);
+}
+
+document.getElementById('player1'), function change() {
+    let player1 = getRandomNumber();
 }
