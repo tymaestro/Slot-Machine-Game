71d7b8bc16df348e3fec62c552856cea5b5c480c
tymaestro <tymaestro22@gmail.com>
2021-11-06 17:56:44 +0000

    edit variables and add initial function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 let playerOne = document.getElementById('player1');
-let playerOne = document.getElementById('player2');
-let playerOne = document.getElementById('player3');
-let computerOne = document.getElementById('player1');
-let computerTwo = document.getElementById('player2');
-let computerThree = document.getElementById('player3');
+let playerTwo = document.getElementById('player2');
+let playerThree = document.getElementById('player3');
+let computerOne = document.getElementById('computer1');
+let computerTwo = document.getElementById('computer2');
+let computerThree = document.getElementById('computer3');
+
+function numbers() {
+    let num1 = Math.floor(Math.random() * 3);
+    let num2 = Math.floor(Math.random() * 3);
+    let num3 = Math.floor(Math.random() * 3);
+    let num4 = Math.floor(Math.random() * 3);
+    let num5 = Math.floor(Math.random() * 3);
+    let num6 = Math.floor(Math.random() * 3);
+}
