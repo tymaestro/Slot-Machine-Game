25f24f4994b27a7c1ba38162e2ff2d29d4f88462
tymaestro <tymaestro22@gmail.com>
2021-11-07 10:35:09 +0000

    modify if statement in numbers function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,6 @@ let playerThree = document.getElementById('player3');
 let computerOne = document.getElementById('computer1');
 let computerTwo = document.getElementById('computer2');
 let computerThree = document.getElementById('computer3');
-let playerAnswer = playerOne + playerTwo + playerThree;
-let computerAnswer = computerOne + computerTwo + computerThree;
 
 // Random number generator function and output to HTML
 
@@ -19,22 +17,28 @@ function numbers() {
     let num5 = Math.floor(Math.random() * 3) + 1;
     let num6 = Math.floor(Math.random() * 3) + 1;
 
+    let playerAnswer = num1 + num2 + num3;
+    let computerAnswer = num4 + num5 + num6;
+
     playerOne.innerText = num1;
 	playerTwo.innerText = num2;
 	playerThree.innerText = num3;
 	computerOne.innerText = num4;
 	computerTwo.innerText = num5;
 	computerThree.innerText = num6;
+
+    if (playerAnswer > computerAnswer) {
+        alert("Congratulations! You're a winner!");
+    } else if (playerAnswer === computerAnswer) {
+        alert("It's a draw!")
+    }
+        else {
+        alert("Sorry, better luck next time!");
+    }
 }
 
 // Check answer function and alert
 
-function check() {
-    if (playerAnswer === computerAnswer) {
-        alert("Congratulations! You're a winner!");
-    } else {
-        alert("Sorry, better luck next time!");
-    }
-}
+
 
 // Score section
