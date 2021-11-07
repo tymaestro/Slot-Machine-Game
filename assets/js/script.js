f65f71142bf5526d2a141a7a074f0731fc8aaf5a
tymaestro <tymaestro22@gmail.com>
2021-11-07 09:49:40 +0000

    add check answer function, alerts, and javascript comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
+// Variables declaration section
+
 let playerOne = document.getElementById('player1');
 let playerTwo = document.getElementById('player2');
 let playerThree = document.getElementById('player3');
 let computerOne = document.getElementById('computer1');
 let computerTwo = document.getElementById('computer2');
 let computerThree = document.getElementById('computer3');
+let playerAnswer = playerOne + playerTwo + playerThree;
+let computerAnswer = computerOne + computerTwo + computerThree;
+
+// Random number generator function and output to HTML
 
 function numbers() {
-    let num1 = Math.floor(Math.random() * 3);
-    let num2 = Math.floor(Math.random() * 3);
-    let num3 = Math.floor(Math.random() * 3);
-    let num4 = Math.floor(Math.random() * 3);
-    let num5 = Math.floor(Math.random() * 3);
-    let num6 = Math.floor(Math.random() * 3);
+    let num1 = Math.floor(Math.random() * 3) + 1;
+    let num2 = Math.floor(Math.random() * 3) + 1;
+    let num3 = Math.floor(Math.random() * 3) + 1;
+    let num4 = Math.floor(Math.random() * 3) + 1;
+    let num5 = Math.floor(Math.random() * 3) + 1;
+    let num6 = Math.floor(Math.random() * 3) + 1;
 
     playerOne.innerText = num1;
 	playerTwo.innerText = num2;
@@ -20,3 +26,15 @@ function numbers() {
 	computerTwo.innerText = num5;
 	computerThree.innerText = num6;
 }
+
+// Check answer function and alert
+
+function check() {
+    if (playerAnswer === computerAnswer) {
+        alert("Congratulations! You're a winner!");
+    } else {
+        alert("Sorry, better luck next time!");
+    }
+}
+
+// Score section
